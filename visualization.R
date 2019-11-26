c688ee77a3d73c6c64ad96f9576220d25994cd9e
Weifeng Zhong <weifeng@weifengzhong.com>
2019-11-26 15:56:11 -0500

    daily update

--- visualization.R
+++ visualization.R
@@ -18,7 +18,7 @@ source("src/visualization_functions.r")
 
 ## Setting and default value
 extrafont::loadfonts(device="win")
-current = as.Date("2019-11-26")
+current = as.Date("2019-11-27")
 version = "0.2.0"
 hh = 4.5
 
